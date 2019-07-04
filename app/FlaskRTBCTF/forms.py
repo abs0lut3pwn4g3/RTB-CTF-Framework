24571d3be9b64ef333743e755cf1b51afd56ac3c
eshaan7 <ishubansal2000@gmail.com>
2019-07-05 02:46:18 +0530

    updated username field

--- app/FlaskRTBCTF/forms.py
+++ app/FlaskRTBCTF/forms.py
@@ -15,7 +15,7 @@ class RegistrationForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=4, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
