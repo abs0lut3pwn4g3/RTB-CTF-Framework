8d0e635bb0aacd5ce58414bc20833749261367da
eshaan7 <ishubansal2000@gmail.com>
2019-07-05 02:46:38 +0530

    added __repr__ for Users Model

--- app/FlaskRTBCTF/models.py
+++ app/FlaskRTBCTF/models.py
@@ -13,6 +13,10 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
     
 class Score(db.Model):
     userid=db.Column(db.Integer, primary_key=True)
