16a8a881763fc91debcbdd48310e00f14fee868c
eshaan7 <ishubansal2000@gmail.com>
2019-07-21 23:40:06 +0530

    updated Debug conf

--- src/FlaskRTBCTF/config.py
+++ src/FlaskRTBCTF/config.py
@@ -2,15 +2,15 @@ import os
 
 class Config:
     SECRET_KEY = 'you-will-never-guess' # os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' #os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' # os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    DEBUG = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
-debugState=True
 ctfname = "RootTheBox CTF"
 RunningTime = '{ "from": "3:00 PM 7th July 2019", "to": "12:00 AM 8th July 2019", "TimeZone": "IST" }'
 box = '{ "name": "My Awesome Pwnable Box", "ip": "127.0.0.1", "os": "Linux", "points": { "user": 10, "root": 20 }, "hardness": "You tell" }'
--- src/run.py
+++ src/run.py
@@ -1,7 +1,6 @@
 from FlaskRTBCTF import create_app
-from FlaskRTBCTF.config import debugState
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False or debugState)
+    app.run()
