08c44213481ba2e0ad3a1d067b85344afb5d5481
eshaan7 <ishubansal2000@gmail.com>
2019-07-22 17:36:30 +0530

    more info for config and using datetime module to set running time #3

--- src/FlaskRTBCTF/config.py
+++ src/FlaskRTBCTF/config.py
@@ -1,4 +1,6 @@
 import os
+from datetime import datetime
+import pytz
 
 ''' Flask related Configurations. Note: DO NOT FORGET TO CHANGE SECRET_KEY ! '''
 
@@ -6,7 +8,7 @@ class Config:
     SECRET_KEY = 'you-will-never-guess' # os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' # os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
-    DEBUG = False # Turn DEBUG OFF before deployment
+    DEBUG = True # Turn DEBUG OFF before deployment
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
@@ -15,9 +17,30 @@ class Config:
 
 ''' CTF related Configuration '''
 
+# Specify CTF Name
+
 ctfname = "RootTheBox CTF"
-RunningTime = '{ "from": "3:00 PM 7th July 2019", "to": "12:00 AM 8th July 2019", "TimeZone": "IST" }'
-box = '{ "name": "My Awesome Pwnable Box", "ip": "127.0.0.1", "os": "Linux", "points": { "user": 10, "root": 20 }, "hardness": "You tell" }'
+
+# Specify CTFs Running Time
+
+RunningTime = { 
+    "from": datetime(2019,7,7,15,00,00,0, pytz.timezone('Asia/Calcutta')), 
+    "to": datetime(2019,7,8,0,00,00,0, pytz.timezone('Asia/Calcutta')), 
+    "TimeZone": "IST"
+} # Use `pytz.utc` for UTC timezone
+
+# Speicfy Your Pwnable Box/Machine settings
+
+box = { 
+    "name": "My Awesome Pwnable Box", 
+    "ip": "127.0.0.1", "os": 
+    "Linux", 
+    "points": { "user": 10, "root": 20 }, 
+    "hardness": "You tell" 
+}
+
+# Specify The Hashes, you can use secrets model to generate them
+
 userHash = 'A'*32 # dummy hash, length = 32 fixed
 rootHash = 'B'*32 # dummy hash, length = 32 fixed
 userScore = 10
