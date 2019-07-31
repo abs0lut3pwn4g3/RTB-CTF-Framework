fc490f38febb2d0773503a3cd5a2ba391e93d8ba
eshaan7 <ishubansal2000@gmail.com>
2019-07-31 13:59:10 +0530

    Logging related errors fixed | #7

--- src/create_db.py
+++ src/create_db.py
@@ -1,43 +1,48 @@
-import datetime
+from datetime import datetime
 
 from FlaskRTBCTF import create_app, db, bcrypt
-from FlaskRTBCTF.models import User, Score, Notification
-from FlaskRTBCTF.config import organization
+from FlaskRTBCTF.models import User, Score, Notification, Logs
+from FlaskRTBCTF.config import organization, LOGGING
 
 app = create_app()
 
 # create_app().app_context().push()
 with app.app_context():
-    db.create_all()
+	db.create_all()
 
-    # NOTE: CHANGE DEFAULT CREDENTIALS !!!
-    admin_user = User(
-        username='admin',
-        email='admin@admin.com',
-        password=bcrypt.generate_password_hash('admin').decode('utf-8'),
-        confirmed_at=datetime.datetime.now(),
-        isAdmin = True,
-        visitedMachine = True
-    )
-    admin_score = Score(user=admin_user, userHash=False, rootHash=False, points=0)
-    db.session.add(admin_user)
-    db.session.add(admin_score)
+	default_time = datetime.utcnow()
 
-    notif = Notification(
-        title=f"Welcome to {organization['ctfname']}",
-        body = "The CTF is live now. Please read rules!"
-    )
-    db.session.add(notif)
+	# NOTE: CHANGE DEFAULT CREDENTIALS !!!
+	admin_user = User(
+		username='admin',
+		email='admin@admin.com',
+		password=bcrypt.generate_password_hash('admin').decode('utf-8'),
+		isAdmin = True
+	)
+	admin_score = Score(user=admin_user, userHash=False, rootHash=False, points=0)
+	db.session.add(admin_user)
+	db.session.add(admin_score)
 
-    '''    
-    test = User(
-        username='test',
-        email='test@test.com',
-        password=bcrypt.generate_password_hash('test').decode('utf-8'),
-    )
-    testscore = Score(user=test, userHash=False, rootHash=False, points=0)
-    db.session.add(test)
-    db.session.add(testscore)
-    '''
+	notif = Notification(
+		title=f"Welcome to {organization['ctfname']}",
+		body = "The CTF is live now. Please read rules!"
+	)
+	db.session.add(notif)
 
-    db.session.commit()
+	test_user = User(
+		username='test',
+		email='test@test.com',
+		password=bcrypt.generate_password_hash('test').decode('utf-8')
+	)
+	test_score = Score(user=test_user, userHash=False, rootHash=False, points=0)
+	db.session.add(test_user)
+	db.session.add(test_score)
+	
+	if LOGGING:
+		admin_log = Logs(user=admin_user, accountCreationTime=default_time, 
+							visitedMachine=True, machineVisitTime=default_time)
+		db.session.add(admin_log)
+		test_log = Logs(user=test_user, accountCreationTime=default_time)
+		db.session.add(test_log)
+
+	db.session.commit()
