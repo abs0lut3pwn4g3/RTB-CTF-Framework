36b19c8a70f8c56d574c9581d824f1d2eb5bdcd3
eshaan7 <ishubansal2000@gmail.com>
2019-07-31 13:59:23 +0530

    name typo

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 WORKERS=4 # change here to the change number of workers
 
-echo "Starting RTB-CTFd"
+echo "Starting RTB-CTF-Framework"
 exec gunicorn 'FlaskRTBCTF:create_app()' \
     --bind '0.0.0.0:8080' \
     --workers $WORKERS
