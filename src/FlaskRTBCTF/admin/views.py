f438beb5574baea37513a7ac7ce458f5ac0c4dfc
Eshaan Bansal <eshaan7bansal@gmail.com>
2020-04-27 00:38:01 +0530

    new theme for admin views, search functionality

--- src/FlaskRTBCTF/admin/views.py
+++ src/FlaskRTBCTF/admin/views.py
@@ -1,13 +1,16 @@
 """ Admin Model Views. """
 
-from flask import abort
+from flask import abort, redirect, flash
 from flask_login import current_user
+from flask_admin import expose
+from flask_admin.form import SecureForm
 from flask_admin.contrib.sqla import ModelView
 
 
-class MyModelView(ModelView):
-
-    column_exclude_list = ("password",)
+class BaseModelView(ModelView):
+    export_types = ("csv", "json")
+    can_export = True
+    form_base_class = SecureForm
 
     def is_accessible(self):
         if not current_user.is_authenticated or not current_user.isAdmin:
@@ -25,3 +28,20 @@ class MyModelView(ModelView):
             if current_user.is_authenticated:
                 # permission denied
                 abort(403)
+
+
+class UserAdminView(BaseModelView):
+    column_exclude_list = ("password",)
+    column_searchable_list = ("username", "email")
+
+    @expose("/new/")
+    def create_view(self):
+        flash("Please use registration form for creating new users.", "info")
+        return redirect("/admin/user")
+
+
+class MachineAdminView(BaseModelView):
+    column_searchable_list = ("name", "ip")
+    form_choices = {
+        "hardness": [("easy", "Easy"), ("medium", "Medium"), ("hard", "Hard")]
+    }
