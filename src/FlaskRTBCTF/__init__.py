2908f96c883be75b5329001f46eeef3d47728bbd
Eshaan Bansal <eshaan7bansal@gmail.com>
2020-04-30 21:25:29 +0530

    admin views init moved into utils/

--- src/FlaskRTBCTF/__init__.py
+++ src/FlaskRTBCTF/__init__.py
@@ -3,12 +3,6 @@ import os
 from flask import Flask
 
 from FlaskRTBCTF.config import Config
-from FlaskRTBCTF.admin.views import (
-    BaseModelView,
-    UserAdminView,
-    MachineAdminView,
-    NotificationAdminView,
-)
 from FlaskRTBCTF.utils import (
     db,
     bcrypt,
@@ -18,36 +12,23 @@ from FlaskRTBCTF.utils import (
     mail,
     inject_app_context,
 )
-
-from FlaskRTBCTF.users.models import User, Logs
-from FlaskRTBCTF.main.models import Notification
-from FlaskRTBCTF.ctf.models import Machine
-
 from FlaskRTBCTF.users.routes import users
 from FlaskRTBCTF.ctf.routes import ctf
 from FlaskRTBCTF.main.routes import main
 
+
 _blueprints = (users, ctf, main)
+
+_extensions = (db, bcrypt, cache, login_manager, admin_manager, mail)
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-
-    db.init_app(app)
-    bcrypt.init_app(app)
-    cache.init_app(app)
-    login_manager.init_app(app)
-    admin_manager.init_app(app)
-    mail.init_app(app)
-
     app.context_processor(inject_app_context)
 
-    # Add model views for admin control
-    admin_manager.add_view(UserAdminView(User, db.session))
-    admin_manager.add_view(MachineAdminView(Machine, db.session))
-    admin_manager.add_view(NotificationAdminView(Notification, db.session))
-    admin_manager.add_view(BaseModelView(Logs, db.session))
+    for _ext in _extensions:
+        _ext.init_app(app)
 
     for _bp in _blueprints:
         app.register_blueprint(_bp)
